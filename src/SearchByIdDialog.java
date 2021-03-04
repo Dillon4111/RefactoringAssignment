76cb38701fa0e3e2ee83e32825247a6f6fd17c93
Dillon Rochford <dillonrochford@gmail.com>
2021-03-04 16:19:33 +0000

    Add images for navigation

--- src/SearchByIdDialog.java
+++ src/SearchByIdDialog.java
@@ -10,15 +10,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
+import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 
 public class SearchByIdDialog extends JDialog implements ActionListener {
@@ -59,6 +51,8 @@ public class SearchByIdDialog extends JDialog implements ActionListener {
         searchField.setDocument(new JTextFieldLimit(20));
 
         buttonPanel.add(search = new JButton("Search"));
+//        new JButton(new ImageIcon(
+//                new ImageIcon("first.png").getImage().getScaledInstance(17, 17, java.awt.Image.SCALE_SMOOTH))));
         search.addActionListener(this);
         search.requestFocus();
 
